9b5b7c6de185ad5a7beb280d930ba45498aadbf4
Ryan Cullen <ryantcullen5@gmail.com>
2020-12-21 16:26:29 -0800

    Update threedoors.py

--- threedoors.py
+++ threedoors.py
@@ -1,16 +1,23 @@
 import numpy as np
 import math
 
+# tracks how many times switching was the correct move to make
 numCorrect = 0
+
+# the number of trials to run the experiment
 trials = 10000
 
 for i in range(trials):
 
+    # list of doors
     doorz = [0, 0, 0]
-
+    
+    # randomize the user's initial choice
     choiceIndex = np.random.randint(0,3)
+    # random index to represent the door with the car behind it
     carIndex = np.random.randint(0,3)
 
+    # set list element with the car to 1
     doorz[carIndex] = 1
 
     #handle elimination of one of the doors and then switching
@@ -38,10 +45,13 @@ for i in range(trials):
         
 
     if choiceIndex == carIndex:
+        # if switching was successful, increment numCorrect
         numCorrect += 1
 
+# calculate the precentage of successful switches to total trials
 precentCorrect = numCorrect/trials
 
+# display result
 print(precentCorrect)
     
     
